5015a4294389c29b8b6f24174d9108b3735497e6
atocheniy <atochony@gmail.com>
2025-10-20 17:31:26 +0300

    add Interface1

---
+++ WPF_Pages/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Pages
+{
+    public interface Interface1
+    {
+        void Print(string text);
+        void Print(string text, params object[] args);
+        void Print();
+    }
+}
